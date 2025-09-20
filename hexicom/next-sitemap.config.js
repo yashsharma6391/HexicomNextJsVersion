fb3967aa9a22894903e04e04aec29cfa99e2c0c7
yashsharma6391 <ys7097569@gmail.com>
2025-09-20 14:40:57 +0530

    Update next-sitemap.config.js

--- hexicom/next-sitemap.config.js
+++ hexicom/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://wwww.hexicom.in',   // <-- change to your real domain
+  siteUrl: 'https://www.hexicom.in',   // <-- change to your real domain
   generateRobotsTxt: true,              // also create robots.txt automatically
   sitemapSize: 7000,                     // optional, splits big sitemaps
   changefreq: 'weekly',
