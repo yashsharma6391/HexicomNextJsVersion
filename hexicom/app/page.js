953d302d04a5d44bb57a08b7bd064c941ff40ebd
yashsharma6391 <ys7097569@gmail.com>
2025-09-20 21:50:09 +0530

    Update page.js

--- hexicom/app/page.js
+++ hexicom/app/page.js
@@ -4,7 +4,7 @@ import Sidebar from '@/src/components/sidebar/Sidebar';
 import Homepage from '@/src/pages/homepage/Homepage';
 import React from 'react'
 export const metadata = {
-    title:"Hexicom – Bridging Technology & Infrastructure",
+    title:"Hexicom Integrated Bridging of Technology & Infrastructure",
     description:"Incorporated in 2025, our company represents the culmination of nearly two decades of industry experience, visionary leadership, and a relentless pursuit of excellence.",
     keywords: ['nextjs', 'seo', 'homepage'],
 }
